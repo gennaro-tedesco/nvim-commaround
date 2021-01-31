201cf259d6baba752e36ef503e92534e597af8d1
Gennaro Tedesco <gennarotedesco@gmail.com>
2021-01-31 02:58:59 +0100

    improved the ifelse condition in toggling blocks

--- lua/exlin/init.lua
+++ lua/exlin/init.lua
@@ -7,8 +7,8 @@ local config = {
 local function toggle_block()
    local comment_start_string = config.block_start
    local comment_end_string = config.block_end
-   vim.cmd('norm `<'..comment_start_string..' ')
-   vim.cmd('norm `>'..comment_start_string..' ')
+   vim.cmd('norm `<i'..comment_start_string..' ')
+   vim.cmd('norm `>a '..comment_end_string)
 end
 
 local function toggle_single()
@@ -19,10 +19,10 @@ end
 local function toggle_comment()
    local line_number_start = vim.api.nvim_exec([[ echo line("\'<") ]], true)
    local line_number_end = vim.api.nvim_exec([[ echo line("\'>") ]], true)
-   if line_number_end > line_number_start then
-	  toggle_block()
-   else
+   if line_number_end == line_number_start then
 	  toggle_single()
+   else
+	  toggle_block()
    end
 end
 
