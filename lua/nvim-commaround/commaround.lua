2c5e35a8db64afe56db3b078f0c04a189273d98c
Gennaro Tedesco <gennarotedesco@gmail.com>
2021-02-01 23:47:37 +0100

    for multi-line block comment, also check if last line is commented

--- lua/nvim-commaround/commaround.lua
+++ lua/nvim-commaround/commaround.lua
@@ -10,7 +10,8 @@ end
 local function is_comment_block_surround(filetype_config, lines)
    local first_line = trim(lines[1])
    local last_line = trim(lines[#lines])
-   return (first_line:sub(1, #filetype_config['block']['left']) == filetype_config['block']['left']) and (last_line:sub(#last_line - #filetype_config['block']['right'] + 1, #last_line) == filetype_config['block']['right'])
+   return (first_line:sub(1, #filetype_config['block']['left']) == filetype_config['block']['left']) and
+		 (last_line:sub(#last_line - #filetype_config['block']['right'] + 1, #last_line) == filetype_config['block']['right'])
 end
 
 local function comment_block_surround(filetype_config)
@@ -20,21 +21,24 @@ end
 
 local function uncomment_block_surround(filetype_config, context)
    context.lines[1] = context.lines[1]:sub(#filetype_config['block']['left'] + 2, #context.lines[1])
-   context.lines[#context.lines] = context.lines[#context.lines]:sub(1, #context.lines[#context.lines] - #filetype_config['block']['right'] - 1)
+   context.lines[#context.lines] = context.lines[#context.lines]:sub(1, #context.lines[#context.lines] -
+		 #filetype_config['block']['right'] - 1)
    vim.api.nvim_buf_set_lines(0, context.start_number, context.end_number, false, context.lines)
 end
 
 local function is_comment_block_multiline(filetype_config, lines)
    local first_line = trim(lines[1])
-   return (first_line:sub(1, #filetype_config['single']) == filetype_config['single'])
+   local last_line = trim(lines[#lines])
+   return (first_line:sub(1, #filetype_config['single']) == filetype_config['single']) and
+		 (last_line:sub(1, #filetype_config['single']) == filetype_config['single'])
 end
 
 local function comment_block_multiline(filetype_config)
-   vim.cmd('\'<,\'>norm 0i'..filetype_config['single'])
+   vim.cmd('\'<,\'>norm 0i'..filetype_config['single']..' ')
 end
 
 local function uncomment_block_multiline(filetype_config)
-   vim.cmd('\'<,\'>norm 0'..#filetype_config['single']..'x')
+   vim.cmd('\'<,\'>norm 0'..#filetype_config['single'] + 1 ..'x')
 end
 
 --[[
